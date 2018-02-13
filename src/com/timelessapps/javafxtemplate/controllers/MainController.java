252f80c8cc654ccee5e81964123b10e9398c6f4d
MT9304 <MT9304@gmail.com>
2018-02-12 22:01:22 -0800

    Renamed project.

--- src/com/timelessapps/javafxtemplate/controllers/MainController.java
+++ src/com/timelessapps/javafxtemplate/controllers/MainController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.ta.javafxbase.controllers;
+package com.timelessapps.javafxtemplate.controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
