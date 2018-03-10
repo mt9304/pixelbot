cd1116942dd9b533daf4dd4eb3a61a83ea4c06cd
MT9304 <MT9304@gmail.com>
2018-03-09 22:11:51 -0800

    Moved activateTab function to LogsPageController.

--- src/com/timelessapps/javafxtemplate/controllers/alwaysdisplayed/TopMenuPaneController.java
+++ src/com/timelessapps/javafxtemplate/controllers/alwaysdisplayed/TopMenuPaneController.java
@@ -11,7 +11,6 @@ import javafx.scene.input.MouseEvent;
 
 public class TopMenuPaneController implements Initializable
 {
-    SceneHelper sceneHelper = new SceneHelper();
     
     @FXML
     private Label pageNameLabel;
@@ -22,37 +21,9 @@ public class TopMenuPaneController implements Initializable
         pageNameLabel.setText(pageName);
     }
     
-    @FXML
-    private void activateTab(MouseEvent event)
-    {
-        String nodeID = sceneHelper.getSourceID(event.getSource());
-        Node eventNode = sceneHelper.getNodeById(nodeID);
-        eventNode.setStyle("-fx-border-width: 0 0 -1 0; "
-                + "-fx-border-color: white white white white;"
-                + " -fx-background-color: white; "
-                + "-fx-background-insets: 0 0 -1 0, 0, 1, 2;"
-                + " -fx-border-radius: 0px;"
-                + "-fx-background-radius: 0 0 0 0;");
-    }
-    
-    //For how to deactivate all tabs remember to look into: https://stackoverflow.com/questions/24986776/how-do-i-get-all-nodes-in-a-scene-in-javafx/24986845 
-    @FXML
-    private void deactivateTabs(MouseEvent event)
-    {
-        String nodeID = sceneHelper.getSourceID(event.getSource());
-        Node eventNode = sceneHelper.getNodeById(nodeID);
-        eventNode.setStyle("-fx-border-width: 0 0 1 0; "
-                + "-fx-border-color: white white black white;"
-                + " -fx-background-color: white; "
-                + "-fx-background-insets: 0 0 -1 0, 0, 1, 2;"
-                + " -fx-border-radius: 0px;"
-                + "-fx-background-radius: 0 0 0 0;");
-    }
-    
     @Override
     public void initialize(URL url, ResourceBundle rb)
     {
-        // TODO
+        
     }    
-    
 }
