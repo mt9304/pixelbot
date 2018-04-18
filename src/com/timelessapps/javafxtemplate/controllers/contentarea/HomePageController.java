39920e7128db4eb250e845ad064b7279833c80f3
MT9304 <MT9304@gmail.com>
2018-04-17 20:42:52 -0700

    Starting on JNI integration.

--- src/com/timelessapps/javafxtemplate/controllers/contentarea/HomePageController.java
+++ src/com/timelessapps/javafxtemplate/controllers/contentarea/HomePageController.java
@@ -5,8 +5,6 @@
  */
 package com.timelessapps.javafxtemplate.controllers.contentarea;
 
-import static com.timelessapps.javafxtemplate.abstractsandenums.Coordinates.X;
-import static com.timelessapps.javafxtemplate.abstractsandenums.Coordinates.Y;
 import com.timelessapps.javafxtemplate.services.RobotService;
 import java.awt.AWTException;
 import java.net.URL;
@@ -40,37 +38,7 @@ public class HomePageController implements Initializable
             public void run() 
             {
                 try {bot = new RobotService();} catch (AWTException ex) {Logger.getLogger(HomePageController.class.getName()).log(Level.SEVERE, null, ex);}
-                
-                bot.delay(2000);
-                //bot.type("Test test TEST 123 !@#", 0);
-                //int startingX = bot.getCurrentMousePosition(X);
-                //int startingY = bot.getCurrentMousePosition(Y);
-                
-                int startingX = 1000;
-                int startingY = 500;
-                
-                System.out.println(bot.getCurrentMousePosition(Y));;
-                System.out.println(bot.getCurrentMousePosition(X));
-                
-                bot.mouseGlide(startingX, startingY, 1000, 0, 100, 100);
-                System.out.println(bot.getCurrentMousePosition(X));
-                System.out.println(bot.getCurrentMousePosition(Y));
-                bot.delay(2000);
-                
-                bot.mouseGlide(startingX, startingY, 1000, 1000, 100, 100);
-                System.out.println(bot.getCurrentMousePosition(X));
-                System.out.println(bot.getCurrentMousePosition(Y));
-                bot.delay(2000);
-                
-                bot.mouseGlide(startingX, startingY, 0, 500, 100, 100);
-                System.out.println(bot.getCurrentMousePosition(X));
-                System.out.println(bot.getCurrentMousePosition(Y));
-                bot.delay(2000);
-                
-                bot.mouseGlide(startingX, startingY, 1900, 500, 100, 100);
-                System.out.println(bot.getCurrentMousePosition(X));
-                System.out.println(bot.getCurrentMousePosition(Y));
-                bot.delay(2000);
+
             }
         };
         
