352fbeba2a029ea759ee4f9797f981c4962c944c
MT9304 <MT9304@gmail.com>
2018-05-03 13:18:08 -0700

    Generated test in wrong place, moved it to tests folder.

---
+++ tests/com/timelessapps/javafxtemplate/helpers/abstractsandenums/RoutineTest.java
@@ -0,0 +1,33 @@
+package com.timelessapps.javafxtemplate.helpers.abstractsandenums;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RoutineTest {
+
+	@Test
+	public void testWaitIfNotRunning() 
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPauseRunning() 
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testStartRunning() 
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testStopRunning() 
+	{
+		fail("Not yet implemented");
+	}
+
+}
