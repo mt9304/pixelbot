a4833ac1fff247c8bff27c25048bc18997ca549a
MT9304 <MT9304@gmail.com>
2018-05-03 17:18:08 -0700

    Fixed build path error.

---
+++ src/test/java/com/timelessapps/javafxtemplate/helpers/abstractsandenums/RoutineTest.java
@@ -0,0 +1,34 @@
+package test.java.com.timelessapps.javafxtemplate.helpers.abstractsandenums;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RoutineTest {
+
+	@Test
+	public void testCheckIfPausedOrStopped() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWaitIfPaused() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPauseRunning() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testStartRunning() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testStopRunning() {
+		fail("Not yet implemented");
+	}
+
+}
