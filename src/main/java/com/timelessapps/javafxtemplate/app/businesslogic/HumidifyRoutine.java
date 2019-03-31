d092845070e61045653a4229875f49d0a7ebbab8
MT9304 <MT9304@gmail.com>
2019-03-30 22:20:15 -0700

    Working on humidify routine.

--- src/main/java/com/timelessapps/javafxtemplate/app/businesslogic/HumidifyRoutine.java
+++ src/main/java/com/timelessapps/javafxtemplate/app/businesslogic/HumidifyRoutine.java
@@ -21,26 +21,35 @@ public class HumidifyRoutine extends Routine {
 	LoggingService log = new LoggingService();
 	Random random = new Random();
 
-	// For arrows, remember to include more in inv than stated number, otherwise
-	// stack will shrink and pixel detector may be off.
-	int numberToAlch = 980;
-	int alchX = 1372; // 1369
-	int alchY = 520; // 546
-
-	int equippedArrowSlotX = 1334;
-	int equippedArrowSlotY = 463;
-
-	int blankInvSlotX = 1165;
-	int blankInvSlotY = 416;
-
-	int firstInvSlotX = 0;
-	int firstInvSlotY = 0;
-
-	// Superheat item red part.
-	int bookIndicatorX = 1313;
-	int bookIndicatorY = 518;
-
-	volatile Boolean bookStillLoading = true;
+	int[] bankX = { 788,800,812 };
+	int[] bankY = { 391,395,410 };
+	
+	int[] closeBankX = { 1084,1093 };
+	int[] closeBankY = { 158,160 };
+	
+	int humidifyX = 1282;
+	int humidifyY = 435;
+	
+	//These are closer to the bank tab and are usually the slots being clicked to deposit. 
+	int[][] firstPriorityClaySlots = 
+		{
+				{1203, 418},{1257, 417},{1307, 417},{1201, 463},{1256, 462},{1204, 508},{1256, 508}
+		};
+	
+	int[][] secondPriorityClaySlots = 
+		{
+				{1363, 416},{1311, 466},{1312, 505},{1308, 551},{1256, 554},{1204, 554}
+		};
+	
+	int[][] thirdPriorityClaySlots = 
+		{
+				{1364, 643},{1362, 554},{1203, 640},{1255, 642},{1202, 685},{1255, 687}
+		};
+	
+	//Indicator for bank tab being open. Looks for the All button to be selected and red. Also needed for routine to work as well. 
+	//Red is: r129 g31 b29 and Grey is: r82 g82 b78
+	int allButtonX = 891;
+	int allButtonY = 540;
 
 	public HumidifyRoutine() throws AWTException {
 
@@ -63,10 +72,11 @@ public class HumidifyRoutine extends Routine {
 					checkIfPausedOrStopped();
 					/** Start routine here. **/
 					// F5 = Inv, F6 = Equipment, F7 = SpellBook.
-					// Move to starting spot.
-					bookStillLoading = true;
+					// Move to starting spot. Open spell book, stand at Edgeville bank, select All for bank option, select clay tab so normal clay is second slot, make astral runes in the last inv slot. 
+					
 					bot.delay(250);
-
+					
+					
 
 					/** End routine here. **/
 					bot.delay(random.nextInt(500) + 500);
@@ -80,24 +90,6 @@ public class HumidifyRoutine extends Routine {
 
 	@Override
 	public void checkIfPausedOrStopped() throws InterruptedException {
-		if (numberToAlch <= 0) {
-			System.out.println("Preparing to shut down. ");
-			running = false;
-			// For sleeping computer.
-			bot.delay(1000);
-			bot.moveCursorTo(35, 1050);
-			bot.delay(1000);
-			bot.mouseClick();
-			bot.delay(1000);
-			bot.moveCursorTo(35, 985);
-			bot.delay(1000);
-			bot.mouseClick();
-			bot.delay(1000);
-			bot.moveCursorTo(35, 811);
-			bot.delay(1000);
-			bot.mouseClick();
-		}
-
 		waitIfPaused();
 		if (!running) {
 			enableHumidifyButton();
@@ -113,6 +105,21 @@ public class HumidifyRoutine extends Routine {
 	private void enableHumidifyButton() {
 		CustomSceneHelper sceneHelper = new CustomSceneHelper();
 		sceneHelper.getNodeById("humidifyButton").setDisable(false);
+	}
+	
+	private void randomlyTakeBreak() {
+		if (random.nextInt(204) == 25) {
+			System.out.println("Moving mouse. ");
+			bot.moveCursorTo(random.nextInt(50) + 1700, random.nextInt(300) + 300);
+			try {
+				Thread.sleep(random.nextInt(4000) + 4000);
+			} catch (InterruptedException e) {
+				System.out.println("Randomly Taking Break ran into an error. "+e);
+			}
+			
+			bot.moveCursorTo((random.nextInt(800) + 500), (random.nextInt(500) + 170));
+			bot.delay(random.nextInt(500) + 500);
+		}
 	}
 
 }
