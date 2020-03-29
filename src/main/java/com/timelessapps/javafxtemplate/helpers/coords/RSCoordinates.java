16dd515798193a278668c22c2b02ead017ffb67d
MT9304 <MT9304@gmail.com>
2020-03-29 09:56:41 -0700

    Added getOffsetX funciton.

--- src/main/java/com/timelessapps/javafxtemplate/helpers/coords/RSCoordinates.java
+++ src/main/java/com/timelessapps/javafxtemplate/helpers/coords/RSCoordinates.java
@@ -89,7 +89,7 @@ public class RSCoordinates
 	}
 	
 	/**
-	 * Finds the top left of the RS game area to use as offset. 
+	 * Finds the top of the RS game area to use for calculating game coordinate offsets. 
 	 * @return the Y coordinate of the top of RS game area
 	 * @throws AWTException 
 	 */
@@ -115,7 +115,7 @@ public class RSCoordinates
 				break;
 			}
 		}
-		
+		//Checks for game area under the black area
 		for (int y = startingY; y < middleY; y++)
 		{
 			int red = bot.getPixelColor(middleX, y).getRed();
@@ -137,56 +137,70 @@ public class RSCoordinates
 	 * @return the X coordinate of the top of RS game area
 	 * @throws AWTException
 	 */
-	public Integer getXOffSet(int startingY) throws AWTException
-	{
-		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		int middleX = (int) screenSize.getWidth()/2;
+	public Integer getXOffset(int y) throws AWTException {
 		Robot bot = new Robot();
+		bot.getPixelColor(0, 0);
 		
-		//Checks for black area right under the moveable frame on top of a window
-		for (int x = 0; x < middleX; x++)
+		int minX = 0;
+		int maxX = 1000; //Screens are 1920x1080, so 1000 is around the halfway mark of the screen. 
+		int midpointX = minX + ((maxX-minX)/2);
+		Color black = new Color(0,0,0);
+		
+		//Binary search style scanning of screen for the starting X coordinate of game area. 
+		for (int i = 0; i < 1000; i++) //Set to 1000 because it should not take over 1000 iterations to find it due to screen size. 
 		{
-			int red = bot.getPixelColor(x, startingY).getRed();
-			int green = bot.getPixelColor(x, startingY).getGreen();
-			int blue = bot.getPixelColor(x, startingY).getBlue();
-			System.out.println(red + " " + green + " " + blue);
-			
-			if (!(red == 0 && green == 0 && blue == 0)) //If not black
+			//System.out.println(i);
+			Color currentColor = bot.getPixelColor(midpointX, y);
+			if (isSameColor(currentColor, black))
 			{
-				return x;
+				Color rightOfCurrentColor = bot.getPixelColor(midpointX+1, y);
+				if (!isSameColor(rightOfCurrentColor, black))
+				{
+					return midpointX+1;
+				}
+				else
+				{
+					minX = midpointX;
+					midpointX = minX + ((maxX-minX)/2);
+				}
+				System.out.println("Current X is black: " + midpointX);
 			}
+			else if (!isSameColor(currentColor, black))
+			{
+				Color leftOfCurrentColor = bot.getPixelColor(midpointX-1, y);
+				if (isSameColor(leftOfCurrentColor, black))
+				{
+					return midpointX;
+				}
+				else
+				{
+					maxX = midpointX;
+					midpointX = minX + ((maxX-minX)/2);
+				}
+				System.out.println("Current X is not black: " + midpointX);
+			}
+			
 		}
 		return null;
 	}
 	
-	private int lookForFirstNonBlackPixelFromLeft(int startingY)
+	public boolean isSameColor(Color firstColor, Color secondColor)
 	{
-		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		int middleX = (int) screenSize.getWidth()/2;
-		Robot bot = new Robot();
-		string rgbStringToFind = getrgbstring(r, g, b);
-		string rgbStringResult = getrgbstring(r, g, b);
-		
-		int x = 0;
-		int red = bot.getPixelColor(x, startingY).getRed();
-		int green = bot.getPixelColor(x, startingY).getGreen();
-		int blue = bot.getPixelColor(x, startingY).getBlue();
-		
-		int redRight = bot.getPixelColor(x+1, startingY).getRed();
-		int greenRight = bot.getPixelColor(x+1, startingY).getGreen();
-		int blueRight = bot.getPixelColor(x+1, startingY).getBlue();
-		
-		if (currentRGBString == rgbStringToFind)
+		if (firstColor.getRed() == secondColor.getRed())
 		{
-			if ()
+			if  (firstColor.getGreen() == secondColor.getGreen())
 			{
-				return 
+				if (firstColor.getBlue() == secondColor.getBlue())
+				{
+					return true;
+				}
 			}
-			x = x/2;
-		}
-		else
+		} 
+		else 
 		{
-			x = x + ((middleX - x) /2);
+			return false;
 		}
+		return false;
 	}
+
 }
